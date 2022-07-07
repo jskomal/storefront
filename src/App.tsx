a0bfdfeeaf5985170fb9c4bb6b5531d5306f206f
Jordan Skomal <jskomal@gmail.com>
2022-07-07 00:23:51 -0600

    Add Storefront to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Storefront from './components/Storefront'
+import { ITEMS } from './data/items'
+import { Titems } from './data/items'
+import { Cart } from './data/types'
 
 const App = () => {
+  const [items, setitems] = useState(ITEMS)
+  const [cart, setCart] = useState<Cart[]>([])
   return (
     <div className='App'>
-      <Header />
+      <Header cart={cart} />
+      <Storefront items={items} setItems={setitems} setCart={setCart} />
     </div>
   )
 }
