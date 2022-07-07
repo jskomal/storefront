9f10a8c7654bb22ce6cf4a7e138827a338d9f473
Jordan Skomal <jskomal@gmail.com>
2022-07-07 00:24:01 -0600

    Add id's to items

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,4 +1,4 @@
-type Titems = {
+export type Titems = {
   imgURL: string
   name: string
   description: string
@@ -6,10 +6,12 @@ type Titems = {
   quantity: number
   price: number
   enchantments?: string[]
+  id: number
 }
 
-export const items: Titems[] = [
+export const ITEMS: Titems[] = [
   {
+    id: 0,
     name: 'Fire Extinguister',
     description: 'If you know, you know',
     type: 'Kitchen',
@@ -19,6 +21,7 @@ export const items: Titems[] = [
       'https://images.unsplash.com/photo-1569436078135-61753b84e3e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80'
   },
   {
+    id: 1,
     name: 'Wand',
     description: 'A staple for all practicioners',
     type: 'Lab',
@@ -28,6 +31,7 @@ export const items: Titems[] = [
       'https://images.unsplash.com/photo-1610466025839-ec6040c347b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
   },
   {
+    id: 2,
     name: 'Magic Ring',
     description: 'Enchanted with a variety of spells',
     type: 'Lab',
@@ -38,6 +42,7 @@ export const items: Titems[] = [
       'https://images.unsplash.com/photo-1496989981497-27d69cdad83e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1231&q=80'
   },
   {
+    id: 3,
     name: 'Leather Duster',
     description: 'Protection from the elements, both natural and supernatural',
     type: 'Bedroom',
@@ -47,6 +52,7 @@ export const items: Titems[] = [
       'https://images.unsplash.com/photo-1621343342508-97a99cbdec5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80'
   },
   {
+    id: 4,
     name: 'Hat',
     description: 'Never leave home without it',
     type: 'Bedroom',
@@ -56,12 +62,13 @@ export const items: Titems[] = [
       'https://images.unsplash.com/photo-1601830976337-e32f60eba315?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80'
   },
   {
+    id: 5,
     name: 'Mouse',
     description: 'The Best Boy',
     type: 'Living Room',
     quantity: 1,
     price: 99999999,
     imgURL:
-      'https://natureworks.com.au/products/garden-statues-decor/themed-entry-statements/foo-dog-roman-stone-female/'
+      'https://target.scene7.com/is/image/Target/GUEST_49092929-1a3d-49bd-a40c-7c19192e2882?wid=488&hei=488&fmt=pjpeg'
   }
 ]
