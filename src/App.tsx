74cfd4be29c0eba24c47541d155f3246f4cf5c2c
Jordan Skomal <jskomal@gmail.com>
2022-07-08 11:48:43 -0600

    Add ability to open cart and add items to cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,67 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+
+// Components
 import Header from './components/Header'
 import Storefront from './components/Storefront'
+import Cart from './components/Cart'
+
+// Data
 import { ITEMS } from './data/items'
-import { Titems } from './data/items'
-import { Cart } from './data/types'
+
+// Types
+import { CartItem } from './data/types'
 
 const App = () => {
-  const [items, setitems] = useState(ITEMS)
-  const [cart, setCart] = useState<Cart[]>([])
+  const [items, setItems] = useState(ITEMS)
+  const [cart, setCart] = useState<CartItem[]>([])
+  const [cartIsOpen, setCartIsOpen] = useState(false)
+  const [errorMsg, setErrorMsg] = useState('You should never see this')
+  const errorMsgRef = useRef<HTMLParagraphElement>(null)
+
+  const addToCart = (e: any) => {
+    setItems((prev) => {
+      const indexToChange = prev.findIndex((item) => item.id === parseInt(e.target.value))
+      prev[indexToChange].quantity--
+      return [...prev]
+    })
+    setCart((prev) => {
+      const newCartItem = items.find((item) => item.id === parseInt(e.target.value))
+      if (newCartItem) {
+        const indexOfFound = prev.findIndex((item) => item.name === newCartItem.name)
+        if (indexOfFound === -1) {
+          const ItemToAdd: CartItem = { ...newCartItem, cartID: cart.length, quantity: 1 }
+          prev.push(ItemToAdd)
+        } else {
+          prev[indexOfFound].quantity++
+        }
+      }
+      return [...prev]
+    })
+  }
+
+  const toggleCartOpen = () => {
+    if (!cart[0]) {
+      errorMsgRef.current?.classList.remove('error-msg-init')
+      errorMsgRef.current?.classList.remove('fade-out')
+      errorMsgRef.current?.classList.add('fade-in')
+      setErrorMsg('Add an item to the cart first!')
+      setTimeout(() => {
+        errorMsgRef.current?.classList.remove('fade-in')
+        errorMsgRef.current?.classList.add('fade-out')
+      }, 4000)
+    } else {
+      setCartIsOpen((prev) => !prev)
+    }
+  }
+
   return (
-    <div className='App'>
-      <Header cart={cart} />
-      <Storefront items={items} setItems={setitems} setCart={setCart} />
+    <div className='app' style={cartIsOpen ? { backgroundColor: 'grey' } : {}}>
+      <Header cart={cart} toggleCartOpen={toggleCartOpen} cartIsOpen={cartIsOpen} />
+      <p className='error-msg-init' ref={errorMsgRef}>
+        {errorMsg}
+      </p>
+      <Storefront items={items} addToCart={addToCart} cart={cart} />
+      {cartIsOpen && cart[0] && <Cart cart={cart} setCart={setCart} />}
     </div>
   )
 }
