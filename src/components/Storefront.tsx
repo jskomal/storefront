1c3340030660e4d83b0bde4a4f6fd7df02bc7c6b
Jordan Skomal <jskomal@gmail.com>
2022-07-08 11:50:48 -0600

    Refactor Cart adding to Storefront

--- src/components/Storefront.tsx
+++ src/components/Storefront.tsx
@@ -1,30 +1,33 @@
 import React from 'react'
-import { Titems } from '../data/items'
+import { CartItem, TItem } from '../data/types'
 import { currencyFormatter } from '../data/currencyFormatter'
-import { Cart } from '../data/types'
 
 type TProps = {
-  items: Titems[]
-  setItems: React.Dispatch<React.SetStateAction<Titems[]>>
-  setCart: React.Dispatch<React.SetStateAction<Cart[]>>
+  items: TItem[]
+  addToCart: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  cart: CartItem[]
 }
 
 const Storefront = (props: TProps) => {
-  const { items, setItems } = props
-  console.log(items)
-  const itemGrid = items.map((item) => (
-    <div className='item-entry' key={item.id}>
-      <img className='grid-image row-item' src={item.imgURL} alt={item.description} />
-      <p className='row-item'>{item.name}</p>
-      <p className='row-item'>{item.description}</p>
-      <p className='row-item'>quantity: {item.quantity}</p>
-      <p className='row-item'> price: {currencyFormatter.format(item.price / 100)}</p>
-      <div className='row-item add-cart-view'>
-        <button>Add to Cart</button>
-        <p>0 in cart</p>
+  const { items, addToCart, cart } = props
+  const itemGrid = items.map((item) => {
+    const cartQuantity = cart.find((element) => element.name === item.name)?.quantity
+    return (
+      <div className='item-entry' key={item.id}>
+        <img className='grid-image row-item' src={item.imgURL} alt={item.description} />
+        <p className='row-item'>{item.name}</p>
+        <p className='row-item'>{item.description}</p>
+        <p className='row-item'>quantity: {item.quantity}</p>
+        <p className='row-item'> price: {currencyFormatter.format(item.price / 100)}</p>
+        <div className='row-item add-cart-view'>
+          <button value={item.id} onClick={(e) => addToCart(e)}>
+            Add to Cart
+          </button>
+          <p>{cartQuantity || '0'} in cart</p>
+        </div>
       </div>
-    </div>
-  ))
+    )
+  })
   return <div className='store-view'>{itemGrid}</div>
 }
 
