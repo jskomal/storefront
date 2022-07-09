752ed7fb89907fa00621733d62f6f42334d299a5
Jordan Skomal <jskomal@gmail.com>
2022-07-09 00:54:57 -0600

    Refactor quantity to cartQuantity where appropriate

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,25 +1,32 @@
 import { currencyFormatter } from '../data/currencyFormatter'
-import { CartItem } from '../data/types'
+import { TItem, CartItem } from '../data/types'
 import CartQuantity from './CartQuantity'
 
 type CartProps = {
   cart: CartItem[]
   setCart: React.Dispatch<React.SetStateAction<CartItem[]>>
+  setCartIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+  setItems: React.Dispatch<React.SetStateAction<TItem[]>>
 }
 
-const Cart = ({ cart, setCart }: CartProps) => {
+const Cart = ({ cart, setCart, setCartIsOpen, setItems }: CartProps) => {
   const cartItems = cart.map((item) => (
     <div key={item.id} className='cart-item'>
       <img className='cart-img' src={item.imgURL} alt={item.name} />
       <div>
         <p>{item.name}</p>
-        <p>{item.quantity} in cart</p>
+        <p>{item.cartQuantity} in cart</p>
       </div>
-      <CartQuantity item={item} setCart={setCart} />
+      <CartQuantity
+        item={item}
+        setCart={setCart}
+        setCartIsOpen={setCartIsOpen}
+        setItems={setItems}
+      />
     </div>
   ))
 
-  const totalSum = cart.reduce((acc, item) => (acc += item.price * item.quantity), 0)
+  const totalSum = cart.reduce((acc, item) => (acc += item.price * item.cartQuantity), 0)
   return (
     <div className='cart-view'>
       {cartItems}
