462255d8416b51d0ba92292957a7a1e43a0826ab
Jordan Skomal <jskomal@gmail.com>
2022-07-09 01:14:22 -0600

    Add error handling for input values less than 0

--- src/components/CartQuantity.tsx
+++ src/components/CartQuantity.tsx
@@ -6,14 +6,31 @@ type CartQuantityProps = {
   setCart: React.Dispatch<React.SetStateAction<CartItem[]>>
   setCartIsOpen: React.Dispatch<React.SetStateAction<boolean>>
   setItems: React.Dispatch<React.SetStateAction<TItem[]>>
+  setCartErrorMsg: React.Dispatch<React.SetStateAction<string>>
 }
 
-const CartQuantity = ({ item, setCart, setCartIsOpen, setItems }: CartQuantityProps) => {
+const CartQuantity = ({
+  item,
+  setCart,
+  setCartIsOpen,
+  setItems,
+  setCartErrorMsg
+}: CartQuantityProps) => {
   const [itemQuantity, setitemQuantity] = useState(item.cartQuantity)
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (parseInt(e.target.value) < 0) {
+      setCartErrorMsg('Please input a number greater or equal than 0.')
+      return
+    } else {
+      setCartErrorMsg('')
+    }
+    setitemQuantity(parseInt(e.target.value))
+  }
+
   const updateCartQuantity = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const prevCartItemQuantity = item.cartQuantity
     setCart((prev) => {
-      const prevCartItemQuantity = item.cartQuantity
       item.cartQuantity = itemQuantity
       if (item.cartQuantity <= 0) {
         setItems((previous) => {
@@ -39,7 +56,7 @@ const CartQuantity = ({ item, setCart, setCartIsOpen, setItems }: CartQuantityPr
         placeholder={item.cartQuantity.toString()}
         value={itemQuantity}
         id={item.id.toString()}
-        onChange={(e) => setitemQuantity(parseInt(e.target.value))}
+        onChange={handleInputChange}
       />
       <button onClick={updateCartQuantity}>Edit Quantity</button>
     </div>
