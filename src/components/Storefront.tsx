7c6b739e1d22ef54c80710b7a5aed3a8c46a44f3
Jordan Skomal <jskomal@gmail.com>
2022-07-09 00:56:03 -0600

    Refactor addToCard to use a target item

--- src/components/Storefront.tsx
+++ src/components/Storefront.tsx
@@ -4,14 +4,14 @@ import { currencyFormatter } from '../data/currencyFormatter'
 
 type TProps = {
   items: TItem[]
-  addToCart: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  addToCart: (target: TItem) => void
   cart: CartItem[]
 }
 
 const Storefront = (props: TProps) => {
   const { items, addToCart, cart } = props
   const itemGrid = items.map((item) => {
-    const cartQuantity = cart.find((element) => element.name === item.name)?.quantity
+    const cartQuantity = cart.find((element) => element.name === item.name)?.cartQuantity
     return (
       <div className='item-entry' key={item.id}>
         <img className='grid-image row-item' src={item.imgURL} alt={item.description} />
@@ -20,9 +20,7 @@ const Storefront = (props: TProps) => {
         <p className='row-item'>quantity: {item.quantity}</p>
         <p className='row-item'> price: {currencyFormatter.format(item.price / 100)}</p>
         <div className='row-item add-cart-view'>
-          <button value={item.id} onClick={(e) => addToCart(e)}>
-            Add to Cart
-          </button>
+          <button onClick={() => addToCart(item)}>Add to Cart</button>
           <p>{cartQuantity || '0'} in cart</p>
         </div>
       </div>
